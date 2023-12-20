8485502248f5d5e119f5518e774eceaef3c5040a
Vulae <potatocrazy42069@gmail.com>
2023-12-20 15:17:57 -0600

    reddit & youtube link, & cleanup

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -29,43 +29,84 @@
 
 </style>
 
-<!-- Huge help: https://codepen.io/finnhvman/pen/abWrPBZ -->
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="fade-in">
+<div class="bg-purple-950 w-full h-full">
+    <!-- Huge help: https://codepen.io/finnhvman/pen/abWrPBZ -->
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" class="fade-in">
     
-    <defs>
+        <defs>
+    
+            <filter id="stars">
+                <feTurbulence baseFrequency="0.2" {seed} />
+                <feColorMatrix values="0 0 0 9 -5
+                                       0 0 0 9 -5
+                                       0 0 0 9 -5
+                                       0 0 0 0 1" />
+                <feBlend in="SourceGraphic" mode="screen" />
+            </filter>
+    
+            <filter id="nebula1">
+                <feTurbulence type="fractalNoise" baseFrequency="0.005" numOctaves="3" {seed} />
+                <feColorMatrix values="0.3 0 0 0 0
+                                       0 0 0 0 0
+                                       0 0 1.2 0 0
+                                       0.5 -0.5 0.5 0 0" />
+            </filter>
+    
+            <filter id="nebula2">
+                <feTurbulence type="fractalNoise" baseFrequency="0.002" numOctaves="3" seed={seed+1} />
+                <feColorMatrix values="0 0 0 0 0
+                                       0 0 0 0 0
+                                       0 0 0.5 0 0
+                                       0.5 -0.5 0.2 0 0" />
+            </filter>
+            
+            <!-- A perspective effect to immerse more into space instead of just being a flat wallpaper. -->
+            <!-- https://www.smashingmagazine.com/2021/09/deep-dive-wonderful-world-svg-displacement-filtering/ -->
+            <!-- https://garden.bradwoods.io/notes/svg/filters/fedisplacementmap -->
+            <linearGradient id="displacement-x" x1="0" x2="1" y1="0" y2="0" color-interpolation="sRGB" gradientUnits="objectBoundingBox">
+                <stop offset="0%" stop-color="#F00" stop-opacity="1" />
+                <stop offset="100%" stop-color="#F00" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient id="displacement-y" x1="0" x2="0" y1="0" y2="1" color-interpolation="sRGB" gradientUnits="objectBoundingBox">
+                <stop offset="0%" stop-color="#00F" stop-opacity="1" />
+                <stop offset="100%" stop-color="#00F" stop-opacity="0" />
+            </linearGradient>
+            <radialGradient id="displacement-circle">
+                <stop offset="0%" stop-color="rgba(127, 127, 127, 1)" />
+                <stop offset="75%" stop-color="rgba(127, 127, 127, 1)" />
+                <stop offset="100%" stop-color="rgba(127, 127, 127, 0)" />
+            </radialGradient>
+            <g id="displacement" class="[&>*]:mix-blend-screen">
+                <rect width="100%" height="100%" fill="black" />
+                <rect width="100%" height="100%" fill="url(#displacement-x)" />
+                <rect width="100%" height="100%" fill="url(#displacement-y)" />
+                <circle cx="50%" cy="50%" r="50%" fill="url(#displacement-circle)" />
+            </g>
+            <filter id="perspective">
+                <feImage href="#displacement" result="displace" />
+                <!-- I for the life of me cannot figure out why this will not displace. -->
+                <!-- Displace the displacement map by the SourceGraphic, but not the other way around??????? -->
+                <feDisplacementMap
+                    in="SourceGraphic"
+                    in2="displace"
+                    xChannelSelector="R"
+                    yChannelSelector="B"
+                    scale="50"
+                />
+            </filter>
+    
+        </defs>
+    
+        <rect width="100%" height="100%" fill="black" />
+        
+        <!-- <g width="100%" height="100%" filter="url(#perspective)"> -->
+            <g width="100%" height="100%" filter="url(#stars)">
+                <rect width="100%" height="100%" filter="url(#nebula1)" />
+                <rect width="100%" height="100%" filter="url(#nebula2)" />
+            </g>
+        <!-- </g> -->
+        
+        <!-- <image href="/github.jpg" width="100%" height="100%" filter="url(#perspective)"> -->
 
-        <filter id="stars">
-            <feTurbulence baseFrequency="0.2" {seed} />
-            <feColorMatrix values="0 0 0 9 -5
-                                   0 0 0 9 -5
-                                   0 0 0 9 -5
-                                   0 0 0 0 1" />
-            <feBlend in="SourceGraphic" mode="screen" />
-        </filter>
-
-        <filter id="nebula1">
-            <feTurbulence type="fractalNoise" baseFrequency="0.005" numOctaves="3" {seed} />
-            <feColorMatrix values="0.3 0 0 0 0
-                                   0 0 0 0 0
-                                   0 0 1.2 0 0
-                                   0.5 -0.5 0.5 0 0" />
-        </filter>
-
-        <filter id="nebula2">
-            <feTurbulence type="fractalNoise" baseFrequency="0.002" numOctaves="3" seed={seed+1} />
-            <feColorMatrix values="0 0 0 0 0
-                                   0 0 0 0 0
-                                   0 0 0.5 0 0
-                                   0.5 -0.5 0.2 0 0" />
-        </filter>
-
-    </defs>
-
-    <rect width="100%" height="100%" fill="black" />
-
-    <g width="100%" height="100%" filter="url(#stars)">
-        <rect width="100%" height="100%" filter="url(#nebula1)" />
-        <rect width="100%" height="100%" filter="url(#nebula2)" />
-    </g>
-
-</svg>
+    </svg>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
     import Background from "$components/Background.svelte";
     import { onMount } from "svelte";
 
+    const fetchData: boolean = true;
+
     const login: string = 'Vulae';
     let url: string = 'https://github.com/Vulae';
     let name: string = login;
@@ -9,6 +11,8 @@
     let bio: string | undefined;
     
     onMount(async () => {
+        if(!fetchData) return;
+
         const req = await fetch(`https://api.github.com/users/${login}`)
         const data = await req.json() as object;
 
@@ -49,7 +53,7 @@
     }
     
     .social-link {
-        @apply flex items-center justify-evenly gap-4 px-8 py-2 drop-shadow-sm transition-all text-white;
+        @apply flex items-center justify-between gap-4 px-8 py-2 drop-shadow-sm transition-all text-white;
 
         & > img {
             @apply w-8;
@@ -66,18 +70,24 @@
 
 </style>
 
-<div class="z-0 absolute w-full h-full bg-purple-950">
+<div class="z-0 absolute w-full h-full">
     <Background />
 </div>
 
 <div class="z-10 flex flex-wrap items-center justify-center gap-16 p-8 max-w-full max-h-full">
     <div class="flex flex-col gap-4">
         <div class="group flex gap-2 px-2 py-4 drop-shadow-outset">
-            <img
-                src={avatar}
-                alt="GitHub Profile"
-                class="w-24 h-24 rounded-full border-2 border-white"
+            <a
+                href="https://www.pixiv.net/en/artworks/99750487"
+                title="Profile Picture Source"
+                class="hover:brightness-125 transition-[filter]"
             >
+                <img
+                    src={avatar}
+                    alt="GitHub Profile"
+                    class="w-24 h-24 rounded-full border-2 border-white"
+                >
+            </a>
             <span class="px-2 font-caveat text-8xl font-semibold name">
                 {name}
             </span>
@@ -96,16 +106,26 @@
         class="flex flex-col gap-2 p-2 bg-white bg-opacity-50 backdrop-blur-lg rounded-lg drop-shadow-md text-white font-segoe text-lg font-semibold"
     >
 
-        <span class="flex justify-center w-full border-b-2 border-white pb-2">Links</span>
+        <span class="flex justify-center w-full border-b-2 border-white pb-2 min-w-[256px]">Links</span>
 
-        <a href={url} target="_blank" class="social-link" title="GitHub profile">
-            <img src="/logos/github-mark.svg" alt="GitHub">
-            GitHub
-        </a>
-        <a href="https://discordapp.com/users/1043398419030482994" target="_blank" class="social-link" title="Add as friend on Discord">
-            <img src="/logos/discord-mark.svg" alt="Discord">
-            Discord
-        </a>
+        <div class="flex flex-col px-8">
+            <a href={url} target="_blank" class="social-link" title="GitHub profile">
+                <img src="/logos/github-mark.svg" alt="GitHub">
+                GitHub
+            </a>
+            <a href="https://discordapp.com/users/1043398419030482994" target="_blank" class="social-link" title="Add as friend on Discord">
+                <img src="/logos/discord-mark.svg" alt="Discord">
+                Discord
+            </a>
+            <a href="https://www.reddit.com/user/vulae_/" target="_blank" class="social-link" title="Reddit profile">
+                <img src="/logos/reddit-mark.svg" alt="Reddit">
+                Reddit
+            </a>
+            <a href="https://www.youtube.com/@vulae" target="_blank" class="social-link" title="YouTube channel">
+                <img src="/logos/youtube-mark.svg" alt="YouTube">
+                YouTube
+            </a>
+        </div>
 
         <span class="mx-auto text-xs font-normal [&>a]:text-blue-800 [&>a]:underline [&>a:hover]:text-blue-600">
             <a href="#logos">Logo attribution</a> • <a href="https://github.com/Vulae/vulae.github.io">Source</a>
@@ -124,11 +144,19 @@
         </span>
         <a href="https://github.com/logos" class="attribution-link">
             <img src="/logos/github-mark.svg" alt="GitHub">
-            GitHub Logo
+            GitHub logo
         </a>
         <a href="https://discord.com/branding" class="attribution-link">
             <img src="/logos/discord-mark.svg" alt="Discord">
-            Discord Logo
+            Discord logo
+        </a>
+        <a href="https://www.redditinc.com/brand" class="attribution-link">
+            <img src="/logos/reddit-mark.svg" alt="Reddit">
+            Reddit logo
+        </a>
+        <a href="https://www.youtube.com/howyoutubeworks/resources/brand-resources/" class="attribution-link">
+            <img src="/logos/youtube-mark.svg" alt="YouTube">
+            YouTube logo
         </a>
         <span class="text-xs font-normal text-black">
             Logos are property of their respective owners.
