836c6523e932b8c50222f817a2f5090b54ed6c8f
Vulae <potatocrazy42069@gmail.com>
2024-02-13 02:56:15 -0600

    add more projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Background from "$components/Background.svelte";
     import Profile from "$components/Profile.svelte";
-    import ProjectCard from "$components/ProjectCard.svelte";
+    import ProjectCard from "./ProjectCard.svelte";
 
 </script>
 
@@ -19,7 +19,7 @@
             For a more extensive list of projects check out my <a href="https://github.com/Vulae?tab=repositories">GitHub</a>.
         </dd>
     </dl>
-    <div class="flex justify-center gap-6">
+    <div class="flex justify-center flex-wrap gap-6">
         <ProjectCard class="w-80 bg-zinc-950">
             <slot:fragment slot="title">
                 <a href="/">vulae.github.io</a>
@@ -31,21 +31,59 @@
                 with <a href="https://tailwindcss.com/">Tailwind CSS</a> —
                 <a href="https://github.com/Vulae/vulae.github.io">Source code</a>.
             </slot:fragment>
-            <div slot="content" class="relative">
-                <Background class="absolute w-full h-full"/>
-                <div class="scale-50">
-                    <Profile />
+            <slot:fragment slot="extra">
+                <div class="relative">
+                    <Background class="absolute w-full h-full"/>
+                    <div class="w-full h-full flex justify-center items-center">
+                        <div class="scale-50">
+                            <Profile />
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </slot:fragment>
+        </ProjectCard>
+        <ProjectCard class="w-80 bg-zinc-950">
+            <slot:fragment slot="title">
+                <a href="https://github.com/Vulae/itch-io-downloader">itch-io-downloader</a>
+            </slot:fragment>
+            <slot:fragment slot="description">
+                Download & play <a href="https://itch.io/">Itch.io</a> games directly from the game's page.
+            </slot:fragment>
+            <slot:fragment slot="extra">
+                <a href="https://github.com/Vulae/itch-io-downloader/releases/latest" target="_blank">
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <img
+                        class="w-full h-full bg-black object-contain"
+                        src="/itch-io-downloader.png"
+                    >
+                </a>
+            </slot:fragment>
+        </ProjectCard>
+        <ProjectCard class="w-80 bg-zinc-950">
+            <slot:fragment slot="title">
+                <a href="https://github.com/Vulae/gmod-bad-apple">gmod-bad-apple</a>
+            </slot:fragment>
+            <slot:fragment slot="description">
+                A bi-level video encoder to play <a href="https://www.youtube.com/watch?v=FtutLA63Cp8">Bad Apple</a> inside <a href="https://gmod.facepunch.com/">Garry's Mod</a>
+                using <a href="https://www.wiremod.com/">Wiremod</a>.
+            </slot:fragment>
+            <slot:fragment slot="extra">
+                <a href="https://www.youtube.com/watch?v=nf00VvO-mEk" target="_blank">
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <img
+                        class="w-full h-full bg-black object-contain"
+                        src="https://img.youtube.com/vi/nf00VvO-mEk/0.jpg"
+                    >
+                </a>
+            </slot:fragment>
         </ProjectCard>
         <ProjectCard class="w-80 bg-zinc-950">
             <slot:fragment slot="title">
                 That's all!
             </slot:fragment>
             <slot:fragment slot="description">
-                I'm sorta new here so there wont me much for a while. . .
+                Check out my <a href="https://github.com/Vulae">GitHub</a> for all of my projects.
             </slot:fragment>
-            <div slot="content" class="relative" />
         </ProjectCard>
     </div>
 </div>
